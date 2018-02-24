872f713e995fe2478b4624d54994093cf98cd403
yuukinokai <sophie_deng97@hotmail.com>
2018-02-24 04:02:25 -0500

    network

---
+++ Assets/Scripts/NetworkDiscovery.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class OverriddenNetworkDiscovery : NetworkDiscovery
+{
+	public override void OnReceivedBroadcast(string fromAddress, string data)
+	{
+		NetworkManager.singleton.networkAddress = fromAddress;
+		NetworkManager.singleton.StartClient();
+	}
+}
