27a34757950a8a4c5ea759206cfd56c84fdf30ba
Alex Chum <chumalexander96@gmail.com>
2018-02-24 02:23:01 -0500

    Test Scene

---
+++ Assets/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 5.0f;
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector2(horizontal*speed, vertical*speed));
+	}
+}
