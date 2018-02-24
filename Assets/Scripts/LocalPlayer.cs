e935d4472b0b8c14ac263f1c5f8a16ff6da1afba
yuukinokai <sophie_deng97@hotmail.com>
2018-02-24 09:49:46 -0500

    name

--- Assets/Scripts/LocalPlayer.cs
+++ Assets/Scripts/LocalPlayer.cs
@@ -3,33 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+
+
 public class LocalPlayer : NetworkBehaviour {
 
-	[SyncVar]
-	public string pname = "player";
 
-	void OnGUI() {
+
+	//[SyncVar (hook = "CmdChangeName")]public string pname = "player" + nid;
+
+	/*void OnGUI() {
 		if (isLocalPlayer) {
-			pname = GUI.TextField (new Rect (10, 10, Screen.height - 40, 100), pname);
-			CmdChangeName (pname);
-
+			pname = GUI.TextField (new Rect (10, Screen.height - 40, 40, 50), pname);
+			if (GUI.Button (new Rect (60, Screen.height - 40, Screen.height - 40, 50), "Change")) { 
+				CmdChangeName (pname);
+			}
 		}
 	}
-	[Command]
+
 	public void CmdChangeName(string newName){
 		pname = newName;
-	}
+	}*/
+
+	[SyncVar] public string pname;
+
 	// Use this for initialization
 	void Start () {
+		
 		if (isLocalPlayer) {
 			GetComponent<Movement>().enabled = true;
+
 		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (isLocalPlayer)
+		this.pname = "Player " + netId.ToString();
+		this.GetComponentInChildren<TextMesh> ().text = pname;
+		/*if (isLocalPlayer) {
 			this.GetComponentInChildren<TextMesh> ().text = pname;
-		
+		}*/
 	}
 }
