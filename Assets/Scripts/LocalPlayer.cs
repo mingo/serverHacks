27b1df9c5d15816e31d9283e93e12c8d380b15d7
yuukinokai <sophie_deng97@hotmail.com>
2018-02-24 14:47:02 -0500

    2 player

--- Assets/Scripts/LocalPlayer.cs
+++ Assets/Scripts/LocalPlayer.cs
@@ -11,7 +11,7 @@ public class LocalPlayer : NetworkBehaviour {
 
 	//[SyncVar (hook = "CmdChangeName")]public string pname = "player" + nid;
 	[SyncVar] public string pname;
-    [SyncVar] public bool tagged;
+    [SyncVar] public bool tagged = false;
     [SyncVar] public bool canBeTagged = true;
     [SyncVar] public float lastTagged;
 
@@ -32,18 +32,31 @@ public class LocalPlayer : NetworkBehaviour {
 		this.pname = newName;
 	}
 
-    [Command] public void CmdChangeTag(bool newTag) {
-        tagged = newTag;
+	[Command] public void CmdChangeTag1(bool value) {
+		this.tagged = value;
+		Debug.Log (pname + tagged.ToString());
     }
 
-    [Command] public void CmdCanBeTagged(bool value)
+	[Command] public void CmdCanBeTagged1(bool value)
     {
-        canBeTagged = value;
+		this.canBeTagged = value;
+
     }
+	[Command] public void CmdChangeTag() {
+		this.tagged = !this.tagged;
+		Debug.Log (pname + tagged.ToString());
+	}
+
+	[Command] public void CmdCanBeTagged()
+	{
+		this.canBeTagged = !this.canBeTagged;
+
+	}
+
 
     [Command] public void CmdTimeTag(float time)
     {
-        lastTagged= time;
+        this.lastTagged= time;
     }
 
 	// Use this for initialization
@@ -51,7 +64,9 @@ public class LocalPlayer : NetworkBehaviour {
 		this.pname = "Player " + netId.ToString();
 		if (isLocalPlayer) {
 			GetComponent<Movement>().enabled = true;
-
+		}
+		if (this.netId.ToString () == "1") {
+			this.CmdChangeTag();
 		}
 	}
 
@@ -59,10 +74,11 @@ public class LocalPlayer : NetworkBehaviour {
 	void Update () {
 		
 		this.GetComponentInChildren<TextMesh> ().text = pname;
+
         /*if (isLocalPlayer) {
 			this.GetComponentInChildren<TextMesh> ().text = pname;
 		}*/
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		/*GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         if (netId.ToString() == "1" && players.Length > 1 && !tagSet) {
             if (startTime > 0 && Time.timeSinceLevelLoad - startTime > 5 && !tagSet) {
                 GameObject player = players[Random.Range(0, players.Length)];
@@ -74,21 +90,46 @@ public class LocalPlayer : NetworkBehaviour {
             }
         } else {
             startTime = -1.0f;
-        }
+        }*/
+		
 
         if (this.tagged)
         {
             this.GetComponent<SpriteRenderer>().color = Color.green;
+			this.GetComponent<Movement> ().speed = 6;
         } else {
             this.GetComponent<SpriteRenderer>().color = Color.white;
+			this.GetComponent<Movement> ().speed = 3;
         }
 
         if (!this.canBeTagged) {
             if (lastTagged + 5 < Time.timeSinceLevelLoad)
             {
-                canBeTagged = true;
+				this.CmdCanBeTagged();
             }
         }
+		bool check1 = false;
+		bool check2 = true;
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		for (int i = 0; i < players.Length; i++) {
+			if (players [i].GetComponent<LocalPlayer> ().tagged) {
+				check1 = true;
+			} else {
+				check2 = false;
+			}
+		}
+
+		if (this.netId.ToString () == "1") {
+			if (check1 == false) {
+				this.CmdChangeTag1 (true);
+			}
+		}
+		if (this.netId.ToString () == "2") {
+			if (check2 == true) {
+				this.CmdChangeTag1 (false);
+			}
+		}
+
 	}
 
 
@@ -98,19 +139,47 @@ public class LocalPlayer : NetworkBehaviour {
         if (go.tag == "Player") {
             LocalPlayer lp = go.GetComponent<LocalPlayer>();
 
-            if (lp.tagged && this.canBeTagged)
-            {
-                Debug.Log("Collision");
-                lp.CmdCanBeTagged(false);
-                this.CmdChangeTag(true);
-                lp.CmdChangeTag(false);
-                lp.CmdTimeTag(Time.timeSinceLevelLoad);
-                Debug.Log(canBeTagged);
-                Debug.Log(tagged);
-                Debug.Log(pname);
-            } else {
-                Debug.Log("else");
-            }        
+			/*if (this.netId.ToString () == "1" && lp.tagged && this.canBeTagged) {
+				this.CmdCanBeTagged ();
+				this.CmdChangeTag ();
+			} else if (this.canBeTagged) {
+				this.CmdCanBeTagged ();
+				this.CmdChangeTag1 (lp.tagged);
+			}*/
+
+			if (lp.netId.ToString () == "1") {
+				this.CmdCanBeTagged();
+				this.CmdChangeTag1 (lp.tagged);
+			}
+			else{
+				this.CmdCanBeTagged();
+				this.CmdChangeTag();
+			}
+
+			
+	
+
+			/*if (lp.tagged && this.canBeTagged) { //if other player is tag and this player can be tagged
+				Debug.Log ("Collision");
+				this.CmdChangeTag (true); //this player is tag
+
+				//this.canBeTagged = false;
+				//Debug.Log (tagged);
+
+			} 
+			else if (this.tagged && lp.canBeTagged) {
+				this.CmdChangeTag (false);
+				this.CmdCanBeTagged(false);
+				this.CmdTimeTag(Time.timeSinceLevelLoad);
+				//this.lastTagged = Time.timeSinceLevelLoad; //time
+				//this.canBeTagged = false; //other player cant be tagged
+				//this.CmdChangeTag(false); //otherplayer is not tag
+				//lp.CmdChangeTag(true);
+				//lp.canBeTagged = false;
+			}
+			else {
+				Debug.Log(pname);
+            }*/        
         }
 
     }
