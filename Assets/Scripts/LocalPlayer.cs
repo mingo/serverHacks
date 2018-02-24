d40d629eaafb80b8595f2b665d6be722e9c72d56
yuukinokai <sophie_deng97@hotmail.com>
2018-02-24 05:24:59 -0500

    name

--- Assets/Scripts/LocalPlayer.cs
+++ Assets/Scripts/LocalPlayer.cs
@@ -5,6 +5,20 @@ using UnityEngine.Networking;
 
 public class LocalPlayer : NetworkBehaviour {
 
+	[SyncVar]
+	public string pname = "player";
+
+	void OnGUI() {
+		if (isLocalPlayer) {
+			pname = GUI.TextField (new Rect (10, 10, Screen.height - 40, 100), pname);
+			CmdChangeName (pname);
+
+		}
+	}
+	[Command]
+	public void CmdChangeName(string newName){
+		pname = newName;
+	}
 	// Use this for initialization
 	void Start () {
 		if (isLocalPlayer) {
@@ -14,6 +28,8 @@ public class LocalPlayer : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (isLocalPlayer)
+			this.GetComponentInChildren<TextMesh> ().text = pname;
 		
 	}
 }
